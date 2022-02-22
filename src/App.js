a24d85993d5be55f58aea081dcbbd4817f87a047
CHRIS_LEE <chrismlee87@gmail.com>
2022-02-22 15:37:48 -0800

    [add] list of timers

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
+import NewTimer from './components/NewTimer'
+import ListTimers from './components/ListTimers'
+
 const store = createStore(reducers);
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>timers app</h1>
+      <div className="App">
+        <h1>timers app</h1>
+        <NewTimer />
+        <ListTimers />
+      </div>
     </Provider>
   );
 }
--- src/components/NewTimer.js
+++ src/components/NewTimer.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import './NewTimer.css'
 
-// We need to import our action to add a new timer
 import { addTimer } from '../actions'
 
 export default function NewTimer() {
@@ -9,7 +9,7 @@ export default function NewTimer() {
   const dispatch = useDispatch()
 
   return (
-    <div>
+    <div className="NewTimer">
       <input
         type='text'
         placeholder="New Timer Name"
